c5954d694e43274ec6d3fd753df95d08452468ab
JadeLuke <jadeluke.booysen@gmail.com>
2025-03-14 11:19:34 +0200

    added hero and skills sections

--- portfolio/src/app/pages/home/home.component.ts
+++ portfolio/src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../UI/shared-ui/nav/nav.component';
+import { SkillsComponent } from '../skills/skills.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, SkillsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
