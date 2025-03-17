7b3a78d7ed205d385748551f88dbb7d4b95b8ccb
JadeLuke <jadeluke.booysen@gmail.com>
2025-03-17 16:26:58 +0200

    added about section

--- portfolio/src/app/pages/home/home.component.ts
+++ portfolio/src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavComponent } from '../../UI/shared-ui/nav/nav.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { ProjectsComponent } from '../projects/projects.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent, SkillsComponent, ProjectsComponent],
+  imports: [NavComponent, SkillsComponent, ProjectsComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
