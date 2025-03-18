e26bda562263320dfb21b1dd6ed522113cbeb50b
JadeLuke <jadeluke.booysen@gmail.com>
2025-03-18 08:51:32 +0200

    added footer

--- portfolio/src/app/pages/home/home.component.ts
+++ portfolio/src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { NavComponent } from '../../UI/shared-ui/nav/nav.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { AboutComponent } from '../about/about.component';
+import { FooterComponent } from '../../UI/shared-ui/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent, SkillsComponent, ProjectsComponent, AboutComponent],
+  imports: [NavComponent, SkillsComponent, ProjectsComponent, AboutComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
