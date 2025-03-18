b1026a9908fff5890c603619caab4fb46ae8b6ab
JadeLuke <jadeluke.booysen@gmail.com>
2025-03-18 15:07:35 +0200

    changed paragraph on home

--- portfolio/src/app/pages/home/home.component.ts
+++ portfolio/src/app/pages/home/home.component.ts
@@ -4,14 +4,23 @@ import { SkillsComponent } from '../skills/skills.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { AboutComponent } from '../about/about.component';
 import { FooterComponent } from '../../UI/shared-ui/footer/footer.component';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent, SkillsComponent, ProjectsComponent, AboutComponent, FooterComponent],
+  imports: [NavComponent, SkillsComponent, ProjectsComponent, AboutComponent, FooterComponent, LoaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  isLoading = true;
+
+  constructor(){
+    setTimeout(() =>{
+   this.isLoading = false
+    },1000)
+
+  }
 }
