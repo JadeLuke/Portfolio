26df0c372233b4996145207cd345f364f3ff0c0b
JadeLuke <jadeluke.booysen@gmail.com>
2025-03-18 10:08:03 +0200

    added scroll into view on nav

--- portfolio/src/app/UI/shared-ui/nav/nav.component.ts
+++ portfolio/src/app/UI/shared-ui/nav/nav.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from '../../../pages/about/about.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [AboutComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
 
+  toAbout(){
+    document.getElementById('about')?.scrollIntoView({behavior: 'smooth'})
+  }
+
+  toSkills(){
+    document.getElementById('skills')?.scrollIntoView({behavior: 'smooth'})
+  }
+
+  toProjects(){
+    document.getElementById('projects')?.scrollIntoView({behavior: 'smooth'})
+  }
 }
